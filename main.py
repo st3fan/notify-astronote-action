bc566424449d16bac581c6b979cd763607d82cff
Stefan Arentz <stefan@arentz.ca>
2020-09-14 12:00:41 +0000

    Initial import

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+
+
+import os
 import requests
 
+
+def notify():
+    headers = { "Authorization": os.getenv("ASTRONOTE_TOKEN") }
+    notification = {
+        "title": "Your coffee is ready",
+        "body": "Double strong, double sweet."
+    }
+    r = requests.post("https://api.astronote.app/1/notify", headers=headers, json=notification)
+    r.raise_for_status()
+
+
 if __name__ == "__main__":
-    r = requests.get("https://api.ipify.org?format=json")
-    print(r.text)
+    notify()
+
 
